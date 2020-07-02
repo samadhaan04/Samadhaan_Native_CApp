e39e7321290753e7487806d4e318d536a1166047
snehil-gupta21 <56683658+snehil-gupta21@users.noreply.github.com>
2020-07-02 16:58:38 +0530

    Update showcomplaint.dart

--- lib/screens/showcomplaint.dart
+++ lib/screens/showcomplaint.dart
@@ -3,22 +3,23 @@ import 'package:smooth_star_rating/smooth_star_rating.dart';
 
 class ShowComplaint extends StatefulWidget {
   static const routeName = '/show-complaint';
-
   @override
   _ShowComplaintState createState() => _ShowComplaintState();
 }
 
-
-
 class _ShowComplaintState extends State<ShowComplaint> {
-
   var rating = 3.0;
   @override
   Widget build(BuildContext context) {
     final complaintId = ModalRoute.of(context).settings.arguments;
     return Scaffold(
       body: Padding(
-        padding: EdgeInsets.all(25.0),
+        padding: const EdgeInsets.only(
+          top: 35,
+          bottom: 15,
+          left: 20,
+          right: 20,
+        ),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           crossAxisAlignment: CrossAxisAlignment.stretch,
@@ -27,25 +28,42 @@ class _ShowComplaintState extends State<ShowComplaint> {
               child: Container(
                 decoration: BoxDecoration(
                   borderRadius: BorderRadius.circular(10.0),
-                  color: Colors.amber,
+                  color: Colors.lime[200],
                 ),
                 child: Column(
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
-                    Text('Complaint'),
+                    Text(
+                      'Complaint',
+                      style: TextStyle(
+                        fontSize: 18,
+                      ),
+                    ),
                     Container(
                       alignment: Alignment(1, 1),
-                      child: RaisedButton(
-                        textColor: Colors.white,
-                        child: Text(
-                          'Attached Image',
-                          style: TextStyle(fontSize: 20.0),
+                      child: Container(
+                        width: MediaQuery.of(context).size.width * 0.22,
+                        padding: const EdgeInsets.symmetric(
+                          horizontal: 4,
                         ),
-                        color: Colors.black,
-                        elevation: 10.0,
-                        onPressed: () {
-                          return _showBottomSheet(context);
-                        },
+                        child: RaisedButton(
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(
+                              8,
+                            ),
+                          ),
+                          textColor: Colors.limeAccent[700],
+                          child: Text(
+                            'Attached Image',
+                            style: TextStyle(fontSize: 12.0),
+                            textAlign: TextAlign.center,
+                          ),
+                          color: Colors.black,
+                          elevation: 10.0,
+                          onPressed: () {
+                            return _showBottomSheet(context);
+                          },
+                        ),
                       ),
                     ),
                   ],
@@ -53,44 +71,96 @@ class _ShowComplaintState extends State<ShowComplaint> {
               ),
             ),
             SizedBox(
-              height: 10.0,
+              height: MediaQuery.of(context).size.height * 0.03,
             ),
             Expanded(
               child: Container(
-                alignment: Alignment.center,
-                child: Text('Hello'),
                 decoration: BoxDecoration(
-                  border: Border.all(
-                    color: Colors.black,
-                  ),
                   borderRadius: BorderRadius.circular(10.0),
-                  shape: BoxShape.rectangle,
-                  //  color: Colors.orangeAccent,
+                  color: Colors.tealAccent[200],
+                ),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    Container(
+                      alignment: Alignment.center,
+                      child: Text(
+                        'Hello',
+                        style: TextStyle(
+                          fontSize: 18,
+                        ),
+                      ),
+                      // decoration: BoxDecoration(
+                      //   border: Border.all(
+                      //     color: Colors.black,
+                      //   ),
+                      //   borderRadius: BorderRadius.circular(
+                      //     10.0,
+                      //   ),
+                      //   shape: BoxShape.rectangle,
+                      //   //  color: Colors.orangeAccent,
+                      // ),
+                    ),
+                    Container(
+                      alignment: Alignment(1, 1),
+                      child: Container(
+                        width: MediaQuery.of(context).size.width * 0.22,
+                        padding: const EdgeInsets.symmetric(
+                          horizontal: 4,
+                        ),
+                        child: RaisedButton(
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(
+                              8,
+                            ),
+                          ),
+                          textColor: Colors.limeAccent[700],
+                          child: Text(
+                            'Attached Image',
+                            style: TextStyle(
+                              fontSize: 12.0,
+                            ),
+                            textAlign: TextAlign.center,
+                          ),
+                          color: Colors.black,
+                          elevation: 10.0,
+                          onPressed: () {
+                            return _showBottomSheet(context);
+                          },
+                        ),
+                      ),
+                    ),
+                  ],
                 ),
               ),
             ),
             SizedBox(
-              height: 20.0,
+              height: MediaQuery.of(context).size.height * 0.07,
             ),
             Expanded(
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.stretch,
                 children: [
                   RaisedButton(
-                    textColor: Colors.white,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(
+                        15,
+                      ),
+                    ),
+                    textColor: Colors.blueGrey[200],
                     child: Text(
                       'FEEDBACK',
                       style: TextStyle(fontSize: 20.0),
                     ),
-                    color: Colors.pink,
+                    color: Colors.pink[900],
                     elevation: 10.0,
                     onPressed: () {
-//                      return _showBottomSheet(context);
+                      return _showBottomSheet(context);
                     },
                   ),
-                  SizedBox(
-                    height: 20.0,
-                  ),
+                  // SizedBox(
+                  //   height: 5.0,
+                  // ),
                   Expanded(
                     child: Center(
                       child: SmoothStarRating(
@@ -100,7 +170,7 @@ class _ShowComplaintState extends State<ShowComplaint> {
                           rating: rating,
                           size: 50.0,
                           isReadOnly: false,
-//                      filledIconData: Icons.blur_off,
+                          // filledIconData: Icons.blur_off,
                           halfFilledIconData: Icons.blur_on,
                           color: Colors.blue,
                           borderColor: Colors.blueAccent,
@@ -125,6 +195,9 @@ class _ShowComplaintState extends State<ShowComplaint> {
         context: cotx,
         builder: (bctx) {
           return Container(
+            padding: const EdgeInsets.only(
+              bottom: 10,
+            ),
             height: MediaQuery.of(context).size.height * 0.75,
             decoration: new BoxDecoration(
               color: Colors.white,
