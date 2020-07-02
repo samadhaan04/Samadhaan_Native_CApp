36b136775fef6f8d57d2e7e4b80020b426caede0
snehil-gupta21 <56683658+snehil-gupta21@users.noreply.github.com>
2020-07-02 16:44:38 +0530

    Update example.dart

--- lib/screens/example.dart
+++ lib/screens/example.dart
@@ -1,4 +1,3 @@
-
 import 'package:faridabad/providers/auth.dart';
 import 'package:faridabad/screens/filecomplaint.dart';
 import 'package:faridabad/screens/home.dart';
@@ -10,39 +9,101 @@ class ExampleScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Center(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          FlatButton.icon(
-            onPressed: () async {
-              try {
-                final result = await Auth().signOut();
-                if (result) {
-                  Navigator.of(context)
-                      .pushReplacementNamed(HomeScreen.routeName);
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            RaisedButton.icon(
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10),
+              ),
+              color: Colors.black,
+              elevation: 10,
+              padding: const EdgeInsets.symmetric(
+                vertical: 10,
+                horizontal: 16,
+              ),
+              onPressed: () async {
+                try {
+                  final result = await Auth().signOut();
+                  if (result) {
+                    Navigator.of(context)
+                        .pushReplacementNamed(HomeScreen.routeName);
+                  }
+                } catch (e) {
+                  print(e);
                 }
-              } catch (e) {
-                print(e);
-              }
-            },
-            icon: Icon(Icons.minimize),
-            label: Text('Sign out'),
-          ),
-          FlatButton(
-            onPressed: () {
-              Navigator.of(context).pushNamed(FileComplaint.routeName);
-            },
-            child: Text('Go To File Complaint'),
-          ),
-          FlatButton(
-            onPressed: () {
-              Navigator.of(context).pushNamed(ShowComplaint.routeName);
-            },
-            child: Text('Go To show Complaint'),
-          ),
-        ],
+              },
+              icon: Icon(
+                Icons.minimize,
+                color: Colors.white,
+              ),
+              label: const Text(
+                'Sign out',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 28,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 15,
+            ),
+            RaisedButton(
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10),
+              ),
+              color: Colors.black,
+              elevation: 10,
+              onPressed: () {
+                Navigator.of(context).pushNamed(FileComplaint.routeName);
+              },
+              child: Container(
+                padding: const EdgeInsets.symmetric(
+                  vertical: 10,
+                  horizontal: 8,
+                ),
+                child: const Text(
+                  'File Complaint',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 28,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 15,
+            ),
+            RaisedButton(
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10),
+              ),
+              color: Colors.black,
+              elevation: 10,
+              onPressed: () {
+                Navigator.of(context).pushNamed(ShowComplaint.routeName);
+              },
+              child: Container(
+                padding: const EdgeInsets.symmetric(
+                  vertical: 10,
+                  horizontal: 8,
+                ),
+                child: const Text(
+                  'Show Complaint',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 28,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
-    ));
+    );
   }
 }
